22a3a522b90df90e18022b883c5a7281ac63e928
LezardSC <Lezardsouscultive@gmail.com>
2024-03-04 14:26:51 +0100

    d01 finished

--- d01/ex07/periodic_table.py
+++ d01/ex07/periodic_table.py
@@ -1,41 +1,69 @@
 import sys
 
-def fill_html_content():
-	content = """
+def fill_css_content():
+	content = "td{ border: 1px solid black; }"
+	return content
+
+def fill_table_rows(elements):
+	table_rows = []
+
+	for element in elements:
+		element_data = element.split(", ")
+		position_data = element_data[0].split('=')[1].strip().split(':')
+		table_row = f"""
+			<tr>
+				<td>{element_data[0].split('=')[0].strip()}</td>
+				<td>{position_data[1].strip()}</td>
+				<td>{element_data[1].split(':')[1].strip()}</td>
+				<td>{element_data[2].split(':')[1].strip()}</td>
+				<td>{element_data[3].split(':')[1].strip()}</td>
+				<td>{element_data[4].split(':')[1].strip()}</td>
+			</tr>
+		"""
+		table_rows.append(table_row)
+
+	return table_rows
+
+def fill_html_content(elements):
+	table_rows = fill_table_rows(elements)
+
+	content = f"""
 		<!DOCTYPE html>
 		<html lang="en">
 		<head>
-			<title>A simple HTML document</title>
+			<title>Periodic Table</title>
+			<link rel="stylesheet" type="text/css" href="periodic_table.css">
 		</head>
 		<body>
-   			<p>Hello World!<p>
+   			<h1><b>Periodic Table</b></h1>
 			<table>
-  			<tr>
-  				<th>Company</th>
-  				<th>Contact</th>
-  				<th>Country</th>
-  			</tr>
-  			<tr>
-  				<td>Alfreds Futterkiste</td>
-  				<td>Maria Anders</td>
-  				<td>Germany</td>
-  			</tr>
-  			<tr>
-  				<td>Centro comercial Moctezuma</td>
-  				<td>Francisco Chang</td>
-  				<td>Mexico</td>
-  			</tr>
-</table> 
+				<tr>
+					<th>Element</th>
+					<th>Position</th>
+					<th>Number</th>
+					<th>Symbol</th>
+					<th>Molar Mass</th>
+					<th>Electron Configuration</th>
+				</tr>
+				{''.join(table_rows)}
+  				
+			</table> 
 		</body>
-		</html>
+	</html>
 	"""
 	return content
 
 def periodic_table():
-	html_content = fill_html_content()
+	with open("periodic_table.txt", "r") as elements_file:
+		elements = [line.strip() for line in elements_file]
+
+	html_content = fill_html_content(elements)
+	css_content = fill_css_content()
 
 	with open("periodic_table.html", "w") as html_file:
 		html_file.write(html_content)
+	with open("periodic_table.css", "w") as css_file:
+		css_file.write(css_content)
 
 if __name__ == "__main__":
 	periodic_table()
