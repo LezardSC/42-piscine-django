5da3e8c472d1c12adcb4fbcc70a8781dbfe1eee3
LezardSC <Lezardsouscultive@gmail.com>
2024-03-12 15:08:45 +0100

    d02 ex00 finished at 99%

--- d02/ex00/render.py
+++ d02/ex00/render.py
@@ -11,20 +11,38 @@ def create_dic_settings(file_path):
 			result_dict[key] = eval(value)
 	return result_dict
 
-def replace_keys_for_values(content, dict):
-	
+def replace_key_words(content, dict):
+	pattern = re.compile(r'\{(\w+)\}')
+
+	def replace(match):
+		key = match.group(1)
+		return str(dict.get(key, f'{{{key}}}'))
+
+	return pattern.sub(replace, content)
 
 def render():
 	if len(sys.argv) != 2:
-		print("temp error args")
+		print("Error\nThe program takes one argument.")
 		return
+	template = sys.argv[1]
+	if not os.path.exists(template):
+		print(f"Error\nFile \'{template}\' does not exist.")
+		return
+	if not template.endswith(".template"):
+		print("Error\nFile must end with \'.template\'.")
+		return
+
 	dict_settings = create_dic_settings('settings.py')
 	
-	with open('myCV.template', 'r') as source:
+	with open(template, 'r') as source:
 		content = source.read()
 
-	with open('myCV.html', 'w') as dest:
-		dest.write(content)
+	rendered_content = replace_key_words(content, dict_settings)
+	filename = os.path.splitext(os.path.basename(template))[0]
+	html_filename = f"{filename}.html"
+
+	with open(html_filename, 'w') as dest:
+		dest.write(rendered_content)
 
 if __name__ == "__main__":
 	render()
--- d02/ex00/settings.py
+++ d02/ex00/settings.py
@@ -1,5 +1,5 @@
 title = "Lizard's resume"
 name = "Lizard"
 surname = "Liz"
-age = 22
+age = "22"
 profession = "C.E.O. of fun"
